fadf3b957c4d1d49ed8f71684def2a6a8f99d992
Wagner Ulisses Gomes <wagnergomes@brq.com>
2023-06-16 14:21:10 -0300

    First commit.

---
+++ src/main/resources/db/migration/V2__insert_person.sql
@@ -0,0 +1,5 @@
+insert into person (id, name) values (1, 'Maria');
+insert into person (id, name) values (2, 'José');
+insert into person (id, name) values (3, 'Paulo');
+insert into person (id, name) values (4, 'Silva');
+insert into person (id, name) values (5, 'Jone');
